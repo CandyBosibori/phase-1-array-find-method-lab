e674bb46906d4a41fa413f92663d4e8aa26cdc5a
Candy <candy.ongaro@student.moringaschool.com>
2023-12-31 12:34:07 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(Array){
+const winningResult = Array.find(item => item.result === "W")
+
+if (winningResult){
+    return winningResult.year
+} else { undefined}
+}
